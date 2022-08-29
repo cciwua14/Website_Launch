9ff21251b5bf22ba1ebf89e1e0fe4aa3bdf480ff
cciwua14 <cciwua@gmail.com>
2022-08-29 16:24:41 +0100

    Third commit Frontend Design almost complete

--- base/forms.py
+++ base/forms.py
@@ -7,7 +7,7 @@ from phonenumber_field.widgets import PhoneNumberPrefixWidget
 class MemeberRegistrationForm(ModelForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'first_name_field','placeholder':'Enter your first name'}), required=False)
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'last_name_field','placeholder':'Enter your last name'}), required=False)
-    phone_no = PhoneNumberField(widget=PhoneNumberPrefixWidget(initial='NG'))
+    phone_no = PhoneNumberField(widget=PhoneNumberPrefixWidget(initial='NG', attrs={'placeholder':'Enter your phone number'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'email_field','placeholder':'Enter your email'}))
     
    
